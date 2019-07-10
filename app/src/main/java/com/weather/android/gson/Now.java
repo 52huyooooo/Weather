7dbc3d0dcb48aaf3096bbc524a4da2dcd9f97385
52huyooooo <1667194501@qq.com>
2019-07-10 15:10:38 +0800

    Forth

---
+++ app/src/main/java/com/weather/android/gson/Now.java
@@ -0,0 +1,15 @@
+package com.weather.android.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Now {
+    @SerializedName("tmp")
+    public String temperture;
+    @SerializedName("cond")
+    public More more;
+
+    public class More{
+        @SerializedName("txt")
+        public String info;
+    }
+}
