287448fc432935a3f6009c9c5b4c285136c871bb
52huyooooo <1667194501@qq.com>
2019-07-10 17:06:33 +0800

    seventh

--- app/src/main/java/com/weather/android/util/ChooseAreaFragment.java
+++ app/src/main/java/com/weather/android/util/ChooseAreaFragment.java
@@ -1,6 +1,7 @@
 package com.weather.android.util;
 
 import android.app.ProgressDialog;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -14,6 +15,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.weather.android.R;
+import com.weather.android.WeatherActivity;
 import com.weather.android.db.City;
 import com.weather.android.db.County;
 import com.weather.android.db.Province;
@@ -80,6 +82,12 @@ public class ChooseAreaFragment extends Fragment {
                 }else if (currentLevel == LEVEL_CITY){
                     selectedCity = cityList.get(position);
                     queryCounties();
+                }else if (currentLevel == LEVEL_COUNTY){
+                    String weatherId = countyList.get(position).getWeatherId();
+                    Intent intent = new Intent(getActivity(), WeatherActivity.class);
+                    intent.putExtra("weather_id",weatherId);
+                    startActivity(intent);
+                    getActivity().finish();
                 }
             }
         });
