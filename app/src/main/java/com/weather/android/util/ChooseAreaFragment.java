061bae17ebc51a09fb9636a96f13f719f428ef32
52huyooooo <1667194501@qq.com>
2019-07-10 15:34:08 +0800

    改了个BUG

--- app/src/main/java/com/weather/android/util/ChooseAreaFragment.java
+++ app/src/main/java/com/weather/android/util/ChooseAreaFragment.java
@@ -57,44 +57,44 @@ public class ChooseAreaFragment extends Fragment {
     private City selectedCity;
     private int currentLevel;
     // methodso
-@Override
-public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState){
-    View view = inflater.inflate(R.layout.choose_area,container,false);
-    titleText = (TextView) view.findViewById(R.id.title_text);
-    backButton = (Button) view.findViewById(R.id.back_button);
-    listView = (ListView) view.findViewById(R.id.list_view);
-    adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
-    listView.setAdapter(adapter);
-    return view;
-}
-@Override
+    @Override
+    public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.choose_area,container,false);
+        titleText = (TextView) view.findViewById(R.id.title_text);
+        backButton = (Button) view.findViewById(R.id.back_button);
+        listView = (ListView) view.findViewById(R.id.list_view);
+        adapter = new ArrayAdapter<>(getContext(),android.R.layout.simple_list_item_1,dataList);
+        listView.setAdapter(adapter);
+        return view;
+    }
+    @Override
     public void onActivityCreated(Bundle savedInstanceState) {
 
-    super.onActivityCreated(savedInstanceState);
-    listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-        @Override
-        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-            if (currentLevel == LEVEL_PROVINCE){
-                selectedProvince = provinceList.get(position);
-                queryCities();
-            }else if (currentLevel == LEVEL_CITY){
-                selectedCity = cityList.get(position);
-                queryCounties();
+        super.onActivityCreated(savedInstanceState);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                if (currentLevel == LEVEL_PROVINCE){
+                    selectedProvince = provinceList.get(position);
+                    queryCities();
+                }else if (currentLevel == LEVEL_CITY){
+                    selectedCity = cityList.get(position);
+                    queryCounties();
+                }
             }
-        }
-    });
-    backButton.setOnClickListener(new View.OnClickListener() {
-        @Override
-        public void onClick(View v) {
-            if (currentLevel ==LEVEL_COUNTY){
-                queryCities();
-            }else if (currentLevel == LEVEL_CITY){
-                queryProvinces();
+        });
+        backButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (currentLevel ==LEVEL_COUNTY){
+                    queryCities();
+                }else if (currentLevel == LEVEL_CITY){
+                    queryProvinces();
+                }
             }
-        }
-    });
-    queryProvinces();
-}
+        });
+        queryProvinces();
+    }
     private void queryProvinces() {
         titleText.setText("中国");
         backButton.setVisibility(View.GONE);
@@ -111,8 +111,8 @@ public View onCreateView(LayoutInflater inflater , ViewGroup container, Bundle s
             String address = "http://guolin.tech/api/china";
             queryFromServer(address, "province");
         }
-}
-private void queryCities() {
+    }
+    private void queryCities() {
         titleText.setText(selectedProvince.getProvinceName());
         backButton.setVisibility(View.VISIBLE);
         cityList = DataSupport.where("provinceid = ?",
@@ -130,12 +130,12 @@ private void queryCities() {
             String address = "http://guolin.tech/api/china"+provinceCode;
             queryFromServer(address, "city");
         }
-}
-private void queryCounties() {
+    }
+    private void queryCounties() {
         titleText.setText(selectedCity.getCityName());
         backButton.setVisibility(View.VISIBLE);
         countyList= DataSupport.where("cityid = ?",
-            String.valueOf(selectedCity.getId())).find(County.class);
+                String.valueOf(selectedCity.getId())).find(County.class);
         if (countyList.size() > 0) {
             dataList.clear();
             for (County county : countyList) {
@@ -150,62 +150,62 @@ private void queryCounties() {
             String address = "http://guolin.tech/api/china"+provinceCode+"/"+cityCode;
             queryFromServer(address, "county");
         }
-}
+    }
 
-private void queryFromServer(String address,final String type){
-showProgressDialog();
-    HttpUtil.sendOKHttpRequest(address, new Callback() {
-        @Override
-        public void onResponse(Call call, Response response)throws IOException {
-            String responseText = response.body().string();
-            boolean result = false;
-            if ("province".equals(type)){
-                result = Utility.handleProvinceResponse(responseText);
-            }else if("city".equals(type)){
-                result = Utility.handleCityResponse(responseText,selectedProvince.getId());
-            }else if ("county".equals(type)){
-                result = Utility.handleCountyResponse(responseText,selectedCity.getId());
+    private void queryFromServer(String address,final String type){
+        showProgressDialog();
+        HttpUtil.sendOKHttpRequest(address, new Callback() {
+            @Override
+            public void onResponse(Call call, Response response)throws IOException {
+                String responseText = response.body().string();
+                boolean result = false;
+                if ("province".equals(type)){
+                    result = Utility.handleProvinceResponse(responseText);
+                }else if("city".equals(type)){
+                    result = Utility.handleCityResponse(responseText,selectedProvince.getId());
+                }else if ("county".equals(type)){
+                    result = Utility.handleCountyResponse(responseText,selectedCity.getId());
+                }
+                if (result){
+                    getActivity().runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            closePragressDialog();
+                            if ("province".equals(type)){
+                                queryProvinces();
+                            }else if ("city".equals(type)){
+                                queryCities();
+                            }else if ("county".equals(type)){
+                                queryCounties();
+                            }
+                        }
+                    });
+                }
             }
-        if (result){
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    closePragressDialog();
-                    if ("province".equals(type)){
-                        queryProvinces();
-                    }else if ("city".equals(type)){
-                        queryCities();
-                    }else if ("county".equals(type)){
-                        queryCounties();
-                    }
-                }
-            });
-        }
-        }
 
-        @Override
-        public void onFailure(Call call, IOException e){
-            getActivity().runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    closePragressDialog();
-                    Toast.makeText(getContext(),"加载失败...",Toast.LENGTH_SHORT).show();
-                }
-            });
+            @Override
+            public void onFailure(Call call, IOException e){
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        closePragressDialog();
+                        Toast.makeText(getContext(),"加载失败...",Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+    }
+    private void showProgressDialog(){
+        if (progressDialog == null){
+            progressDialog = new ProgressDialog(getActivity());
+            progressDialog.setMessage("加载中...");
+            progressDialog.setCanceledOnTouchOutside(false);
         }
-    });
-}
-private void showProgressDialog(){
-    if (progressDialog == null){
-        progressDialog = new ProgressDialog(getActivity());
-        progressDialog.setMessage("加载中...");
-        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
     }
-    progressDialog.show();
-}
-private void closePragressDialog(){
-    if (progressDialog == null){
-        progressDialog.dismiss();
+    private void closePragressDialog(){
+        if (progressDialog == null){
+            progressDialog.dismiss();
+        }
     }
-}
 }
