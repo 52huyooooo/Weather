63021d55b2d2ed92398a2de178da057c660744c5
52huyooooo <1667194501@qq.com>
2019-07-10 11:34:28 +0800

    Second

---
+++ app/src/main/java/com/weather/android/db/Province.java
@@ -0,0 +1,52 @@
+package com.weather.android.db;
+
+import org.litepal.crud.DataSupport;
+
+public class Province extends DataSupport {
+    private int id;
+    private String provinceName;
+    private int provinceCode;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+    public Province(int id, String provinceName) {
+        this.id = id;
+        this.provinceName = provinceName;
+    }
+
+    Province() {
+    }
+
+    public Province(int id, String provinceName, int provinceCode) {
+        this.id = id;
+        this.provinceName = provinceName;
+        this.provinceCode = provinceCode;
+    }
+
+    @Override
+    public String toString() {
+        return provinceName;
+    }
+}
