4bcb68307dd265c4c50da6fd30d9c41816267515
52huyooooo <1667194501@qq.com>
2019-07-11 09:10:32 +0800

    显示天气

--- app/src/main/java/com/weather/android/WeatherActivity.java
+++ app/src/main/java/com/weather/android/WeatherActivity.java
@@ -1,6 +1,8 @@
 package com.weather.android;
 
 import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.os.Build;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -42,6 +44,13 @@ public class WeatherActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        if (Build.VERSION.SDK_INT >= 21){
+            View decorView = getWindow().getDecorView();
+            decorView.setSystemUiVisibility(
+                    View.SYSTEM_UI_FLAG_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE);
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        }
         setContentView(R.layout.activity_weather);
         bingPicImg = (ImageView)findViewById(R.id.bing_pic_img);
         weatherLayout  =(ScrollView) findViewById(R.id.weather_layout);
